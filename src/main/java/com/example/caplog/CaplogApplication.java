e05ccb4f6fe0f529da31c73842630a439349698e
이서진 <seojin-l>
2026-07-30 14:30:04 +0900

    배포 초기 세팅

---
+++ src/main/java/com/example/caplog/CaplogApplication.java
@@ -0,0 +1,13 @@
+package com.example.caplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaplogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaplogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/caplog/CaplogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caplog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaplogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
