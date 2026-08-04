2c6a871faada803834319d8647086203566f8df0
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-08-04 14:37:11 +0900

    feat: 유저별 과거 일정 유사도 검색(retrieval)

--- src/main/java/com/example/caplog/domain/ai/vector/VectorService.java
+++ src/main/java/com/example/caplog/domain/ai/vector/VectorService.java
@@ -2,14 +2,16 @@ package com.example.caplog.domain.ai.vector;
 
 import com.example.caplog.domain.schedule.entity.Schedule;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.qdrant.QdrantVectorStore;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class VectorService {
@@ -33,5 +35,16 @@ public class VectorService {
 
         // 3. Qdrant에 저장
         vectorStore.add(List.of(document));
+    }
+
+    public List<Document> searchScheduleVector(Long userId, String query) {
+        SearchRequest searchRequest = SearchRequest.query(query)
+                .withTopK(5)                                                // 가장 유사한 상위 5개
+                .withSimilarityThreshold(0.5)                               // 유사도 50% 이상
+                .withFilterExpression("user_id == " + userId); // 다른 사용자 데이터 접근 차단
+
+        List<Document> results = vectorStore.similaritySearch(searchRequest);
+        log.info("Qdrant 유사도 검색 (user_id : {}) - {}개", userId, results);
+        return results;
     }
 }
