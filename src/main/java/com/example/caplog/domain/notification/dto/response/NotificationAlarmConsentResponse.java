25128d8e8c3d96887b833ce9388616f785d0b507
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-08-19 21:55:54 +0900

    hotfix: put요청 문제 해결 시도

--- src/main/java/com/example/caplog/domain/notification/dto/response/NotificationAlarmConsentResponse.java
+++ src/main/java/com/example/caplog/domain/notification/dto/response/NotificationAlarmConsentResponse.java
@@ -1,6 +1,9 @@
 package com.example.caplog.domain.notification.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record NotificationAlarmConsentResponse(
+        @JsonProperty("isApproved")
         Boolean isApproved
 ) {
 }
